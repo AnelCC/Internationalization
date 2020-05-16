df940f609de6f1422ff54b44e96b1358d2786c1b
Anel CC <anel.calvo.cervantes@gmail.com>
2020-05-15 20:16:57 -0400

    [Anel][AndArch01] 2. Initial project

--- app/src/main/java/com/anelcc/andarchitect/MainActivity.kt
+++ app/src/main/java/com/anelcc/andarchitect/MainActivity.kt
@@ -2,11 +2,22 @@ package com.anelcc.andarchitect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
     }
 }
